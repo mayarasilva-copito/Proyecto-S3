5b94a479ff175cd468d64c38df42caec3be7a9bd
TuNuevoUsuario <maysilvaespinoza@crackthecode.la>
2025-10-23 11:09:51 -0500

    holaaaaaaaaaaaaaaaaaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/navbar";
-import Portada from "./components/portada";
-import Quienessomos from "./components/quienessomos";
-import Listademascotas from "./components/mascotas";
-import Formulario from "./components/formulario";
-import Tips from "./components/tips";
-import Contactanos from "./components/contactanos";
-import Footer from "./components/footer";
-import Solicitudes from "./components/solicitudes";
-import Carrito from "./components/carrito";
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/navbar'
+import Portada from './components/portada'
+import Quienessomos from './components/quienessomos'
+import Listademascotas from './components/mascotas'
+import Formulario from './components/formulario'
+import Tips from './components/tips'
+import Contactanos from './components/contactanos'
+import Footer from './components/footer'
+import Solicitudes from './components/solicitudes'
+import Carrito from './components/carrito'
 
 function Rutas() {
-  const location = useLocation();
-  const mascotasAdoptadas = location.state?.mascotasAdoptadas || [];
+  const location = useLocation()
+  const mascotasAdoptadas = location.state?.mascotasAdoptadas || []
 
   return (
     <Routes>
       <Route path="/" element={<Portada />} />
-      <Route path="/portada" element={<Portada />} />
       <Route path="/portada" element={<Portada />} />
       <Route path="/Quienessomos" element={<Quienessomos />} />
       <Route path="/Listademascotas" element={<Listademascotas />} />
@@ -32,7 +31,7 @@ function Rutas() {
       <Route path="/solicitudes" element={<Solicitudes />} />
       <Route path="/Contactanos" element={<Contactanos />} />
     </Routes>
-  );
+  )
 }
 
 function App() {
@@ -42,7 +41,7 @@ function App() {
       <Rutas />
       <Footer />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
